fe83555354954824adc3594c49f4168727d7c860
Top-g-hash <atharvasurwase@gmail.com>
2026-04-18 02:38:41 +0530

    component

---
+++ src/runtime/server/composable/PretextText.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: String,
+  width: {
+    type: Number,
+    default: 300,
+  },
+  font: {
+    type: String,
+    default: "16px Inter",
+  },
+});
+
+const { measure } = usePretext();
+
+const layoutResult = computed(() =>
+  measure(props.text!, {
+    width: props.width,
+    font: props.font,
+  }),
+);
+</script>
+
+<template>
+  <div>
+    <div v-for="(line, i) in layoutResult.lines" :key="i">
+      {{ line.text }}
+    </div>
+  </div>
+</template>
