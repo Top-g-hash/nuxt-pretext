08621d26db58c365ef22d123e8ead009a59e3058
Top-g-hash <atharvasurwase@gmail.com>
2026-04-19 01:46:17 +0530

    SSR placeholders in PretextText

--- src/runtime/components/PretextText.vue
+++ src/runtime/components/PretextText.vue
@@ -1,26 +1,14 @@
+<!-- src/runtime/components/PretextText.vue -->
 <script setup lang="ts">
 import { computed } from "vue";
 import { usePretext } from "../composables/usePretext";
+
 const props = defineProps({
-  text: {
-    type: String,
-    required: true,
-  },
-
-  width: {
-    type: Number,
-    default: 300,
-  },
-
-  font: {
-    type: String,
-    default: "16px Inter",
-  },
-
-  tag: {
-    type: String,
-    default: "div",
-  },
+  text: { type: String, required: true },
+  width: { type: Number, default: 300 },
+  font: { type: String, default: "16px Inter" },
+  lineHeight: { type: Number, default: 20 },
+  tag: { type: String, default: "div" },
 });
 
 const { measure } = usePretext();
@@ -29,13 +17,25 @@ const layoutResult = computed(() =>
   measure(props.text, {
     width: props.width,
     font: props.font,
+    lineHeight: props.lineHeight,
   }),
+);
+
+// SSR: reserve height so no layout jump on hydration
+const placeholderStyle = computed(() =>
+  layoutResult.value.height
+    ? {}
+    : { minHeight: `${props.lineHeight}px`, visibility: "hidden" },
 );
 </script>
 
 <template>
-  <component :is="tag">
-    <div v-for="(line, i) in layoutResult.lines" :key="i">
+  <component :is="tag" :style="placeholderStyle">
+    <div
+      v-for="(line, i) in layoutResult.lines"
+      :key="i"
+      :style="{ lineHeight: lineHeight + 'px' }"
+    >
       {{ line.text }}
     </div>
   </component>
