c0a2a5fa264eb8bb8caabe5d3f70fe78f9df88bc
Top-g-hash <atharvasurwase@gmail.com>
2026-04-19 01:41:11 +0530

    register in module

--- src/module.ts
+++ src/module.ts
@@ -1,37 +1,38 @@
+// src/module.ts
 import {
   defineNuxtModule,
   createResolver,
   addPlugin,
   addComponentsDir,
   addImportsDir,
+  addPluginTemplate,
 } from "@nuxt/kit";
 
 export default defineNuxtModule({
-  meta: {
-    name: "nuxt-pretext",
-    configKey: "pretext",
-  },
-
+  meta: { name: "nuxt-pretext", configKey: "pretext" },
   defaults: {
     defaultFont: "16px Inter",
     defaultWidth: 300,
+    defaultLineHeight: 20,
   },
-
   setup(options, nuxt) {
     const resolver = createResolver(import.meta.url);
 
+    // Pass defaults to runtime via runtimeConfig
     nuxt.options.runtimeConfig.public.pretext = {
-      defaultFont: options.defaultFont ?? "16px Inter",
-      defaultWidth: options.defaultWidth ?? 300,
+      defaultFont: options.defaultFont,
+      defaultWidth: options.defaultWidth,
+      defaultLineHeight: options.defaultLineHeight,
     };
 
     addPlugin(resolver.resolve("./runtime/plugin"));
-
-    addImportsDir(resolver.resolve("./runtime/composables"));
-
+    addImportsDir(resolver.resolve("./runtime/composable"));
     addComponentsDir({
       path: resolver.resolve("./runtime/components"),
       prefix: "Pretext",
     });
+
+    // Register directive via plugin
+    addPlugin(resolver.resolve("./runtime/directivePlugin"));
   },
 });
